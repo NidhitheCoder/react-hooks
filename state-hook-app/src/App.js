1f9ea11ac3c49141694c75ad915d9d220f65bfca
NidhitheCoder <nidhpv@gmail.com>
2021-03-16 13:05:34 +0530

    useState with array and useState notes updated

--- state-hook-app/src/App.js
+++ state-hook-app/src/App.js
@@ -1,6 +1,7 @@
 // Example of counter increment with class component
 import './App.css';
-import HookCounterThree from './components/HookCounterThree';
+import HookCounterFour from './components/HookCounterFour';
+// import HookCounterThree from './components/HookCounterThree';
 // import ClassCounter from './components/ClassCounter';
 // import HookCounter from './components/HookCounter';
 // import HookCounterTwo from './components/HookCounterTwo';
@@ -9,7 +10,7 @@ import HookCounterThree from './components/HookCounterThree';
 function App() {
   return (
     <div className="App">
-    <HookCounterThree />
+    <HookCounterFour />
     </div>
   );
 }
