f552fdbaa2915bab9c1fa4d720bbe7c75a5329fd
NidhitheCoder <nidhpv@gmail.com>
2021-03-16 12:43:20 +0530

    using useState with object

--- state-hook-app/src/App.js
+++ state-hook-app/src/App.js
@@ -1,15 +1,15 @@
 // Example of counter increment with class component
 import './App.css';
+import HookCounterThree from './components/HookCounterThree';
 // import ClassCounter from './components/ClassCounter';
 // import HookCounter from './components/HookCounter';
-import HookCounterTwo from './components/HookCounterTwo';
-import ClassCounterTwo from './components/ClassCounterTwo';
+// import HookCounterTwo from './components/HookCounterTwo';
+// import ClassCounterTwo from './components/ClassCounterTwo';
 
 function App() {
   return (
     <div className="App">
-    <HookCounterTwo />
-    <ClassCounterTwo/>
+    <HookCounterThree />
     </div>
   );
 }
