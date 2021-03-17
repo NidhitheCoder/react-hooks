ecaa5fef723158847cc569032061a126223460c2
NidhitheCoder <nidhpv@gmail.com>
2021-03-17 15:40:39 +0530

    fetching data with useEffect part 3 (button click data fetching

--- state-hook-app/src/components/DataFetching.js
+++ state-hook-app/src/components/DataFetching.js
@@ -4,20 +4,29 @@ import axios from "axios";
 function DataFetching() {
   const [post, setPost] = useState({});
   const [id, setId] = useState(1);
+  const [idFromButtonClick, SetIdFromButtonClick] = useState(1);
+
+  const handleClick = () => {
+    SetIdFromButtonClick(id);
+  };
 
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
       .then(res => {
         setPost(res.data);
       })
       .catch(err => {
         console.log(err);
       });
-  }, [id]);
+  }, [idFromButtonClick]);
+
   return (
     <div>
       <input type="text" value={id} onChange={e => setId(e.target.value)} />
+      <button type="button" onClick={handleClick}>
+        Fetch Post
+      </button>
       <div>{post.title}</div>
     </div>
   );
