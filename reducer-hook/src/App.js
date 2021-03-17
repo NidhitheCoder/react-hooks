8a8fd762975a18d445a8e39e85cecb5b18caaa95
NidhitheCoder <nidhpv@gmail.com>
2021-03-17 18:24:02 +0530

    useReducer simple state and action

--- reducer-hook/src/App.js
+++ reducer-hook/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import CounterOne from "./components/CounterOne";
 
 function App() {
   return (
     <div className="App">
-  <h3>Reducer hook</h3>
+      <h3>Reducer hook</h3>
+      <CounterOne />
     </div>
   );
 }
