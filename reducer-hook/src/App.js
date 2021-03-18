f1e832ab8e6ecc4659d309b6927d483f12bc1f9d
NidhitheCoder <nidhpv@gmail.com>
2021-03-18 12:16:24 +0530

    data fetching with useState

--- reducer-hook/src/App.js
+++ reducer-hook/src/App.js
@@ -1,41 +1,34 @@
-import React, { useReducer } from "react";
+import React from "react";
 import "./App.css";
+import DataFetchingOne from "./components/dataFetching/DataFetchingOne";
 // import CounterThree from "./components/CounterThree";
 // import CounterTwo from "./components/CounterTwo";
 // import CounterOne from "./components/CounterOne";
-import ComponentA from "./components/contextWithReducer/ComponentA";
-import ComponentB from "./components/contextWithReducer/ComponentB";
-import ComponentC from "./components/contextWithReducer/ComponentC";
+// import ComponentA from "./components/contextWithReducer/ComponentA";
+// import ComponentB from "./components/contextWithReducer/ComponentB";
+// import ComponentC from "./components/contextWithReducer/ComponentC";
 
-export const CountContext = React.createContext();
+// export const CountContext = React.createContext();
 
-const initialState = 0;
-const reducer = (state, action) => {
-  switch (action) {
-    case "increment":
-      return state + 1;
-    case "decrement":
-      return state - 1;
-    case "reset":
-      return initialState;
-    default:
-      return state;
-  }
-};
+// const initialState = 0;
+// const reducer = (state, action) => {
+//   switch (action) {
+//     case "increment":
+//       return state + 1;
+//     case "decrement":
+//       return state - 1;
+//     case "reset":
+//       return initialState;
+//     default:
+//       return state;
+//   }
+// };
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialState);
   return (
-    <CountContext.Provider
-      value={{ countState: count, countDispatch: dispatch }}
-    >
       <div className="App">
-        Count => {count}
-        <ComponentA />
-        <ComponentB />
-        <ComponentC />
+    <DataFetchingOne />
       </div>
-    </CountContext.Provider>
   );
 }
 
